d3657ceecd89083913d7cc40a77522b0b88cbfd5
Michal Pavlik <pavlik@firma.azet.sk>
2021-03-12 09:11:33 +0100

    fix with calling res.end()

--- index.js
+++ index.js
@@ -4,51 +4,57 @@ var pjson = require('./package.json');
 
 function stash(req, res, next) {
     var originalSend = res.send;
+    var originalEnd = res.end;
+    var originalRender = res.render;
     const START_TAG = '<!-- static -->';
     const END_TAG = '<!-- static-end -->';
     var needStashScript, needUnstashScript = false;
 
     res.send = function (chunk) {
-        if (chunk.toString() && chunk.toString().indexOf(START_TAG) > -1 && chunk.toString().indexOf(END_TAG) > -1 ) {
+        if (chunk.toString() && chunk.toString().indexOf(START_TAG) > -1 && chunk.toString().indexOf(END_TAG) > -1) {
             let stashed = req.headers.cookie ? req.headers.cookie.split("=").pop().split(",") : [];
             var matchAll = chunk.toString().matchAll(/<!-- static -->([\s\S]*?)<!-- static-end -->/gi);
             const arr = [...matchAll];
-            for(var i=0; i<arr.length; i++){
+            for (var i = 0; i < arr.length; i++) {
                 let withTag = arr[i][0];
                 let withoutTag = arr[i][1];
                 let hash = crypto.createHash('md5').update(withoutTag + pjson.version).digest('hex');
                 let replace = '';
-                if (stashed.indexOf(hash) > -1){
+                if (stashed.indexOf(hash) > -1) {
                     needUnstashScript = true;
-                    replace = '<static ref="'+hash+'"></static>';
+                    replace = '<static ref="' + hash + '"></static>';
                 } else {
                     needStashScript = true;
-                    replace = "<!-- static "+hash+" -->"+withoutTag+"<!-- static-end -->";
+                    replace = "<!-- static " + hash + " -->" + withoutTag + "<!-- static-end -->";
                 }
                 chunk = chunk.toString().replace(withTag, replace);
             }
-            let script = "/* cache-html-part "+pjson.version+"*/";
-            if(needStashScript){
+            let script = "/* cache-html-part " + pjson.version + "*/";
+            if (needStashScript) {
                 script += fs.readFileSync(__dirname + '/browser/stash-minified.js', 'utf8');
             }
-            if(needUnstashScript){
+            if (needUnstashScript) {
                 script += fs.readFileSync(__dirname + '/browser/unstash-minified.js', 'utf8');
             }
-            arguments[0] = chunk.toString().replace(/(<\/body>)/, "<script>"+script+"</script>\n\n$1");
+            arguments[0] = chunk.toString().replace(/(<\/body>)/, "<script>" + script + "</script>\n\n$1");
             res.setHeader('Content-Length', chunk.length);
         }
         originalSend.apply(res, arguments);
     };
-    var _render = res.render;
-    res.render = function( view, options, fn ) {
-        if(fn === undefined){
-            _render.call( this, view, options, (err, out) => {
+    res.render = function (view, options, fn) {
+        if (fn === undefined) {
+            originalRender.call(this, view, options, (err, out) => {
                 res.send(out);
-            } );
+            });
         } else {
-            _render.call( this, view, options, fn);
+            originalRender.call(this, view, options, fn);
         }
     };
+    res.end = function (chunk) {
+        res.end = originalEnd;
+        res.send(chunk);
+    };
+
     next();
 }
 
